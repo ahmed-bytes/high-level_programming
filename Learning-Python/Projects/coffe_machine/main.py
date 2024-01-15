56bbcde883752b4ca997a09a5f12c6d88120f72f
Oladapo Opeyemi Ahmed <oladapoahmed5@gmail.com>
2024-01-15 23:54:22 +0100

    Start working on coffee machine simultaton using OOP

--- Learning-Python/Projects/coffe_machine/main.py
+++ Learning-Python/Projects/coffe_machine/main.py
@@ -1,3 +1,15 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+is_Off = False
+
+while not is_Off:
+    user_drink = input("What would you like? (espresso/latte/cappuccino/): ").lower()
+    if user_drink == "off":
+        is_Off = True
+    elif user_drink == "report":
+        CoffeeMaker.report()
+    else:
+        if CoffeeMaker.is_resource_sufficient(user_drink) == False:
+            print("“Sorry there is not enough")
