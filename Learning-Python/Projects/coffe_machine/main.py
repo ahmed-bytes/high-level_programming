410cc71c6e03bb471dc4d1760cc7abaedadb27f1
Ahmed <oladapoahmed5@gmail.com>
2024-01-16 16:36:32 +0100

    Wrote coffee simultation using oop

--- Learning-Python/Projects/coffe_machine/main.py
+++ Learning-Python/Projects/coffe_machine/main.py
@@ -2,14 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+cm = CoffeeMaker()
+mm = MoneyMachine()
+m = Menu()
+
 is_Off = False
 
 while not is_Off:
-    user_drink = input("What would you like? (espresso/latte/cappuccino/): ").lower()
-    if user_drink == "off":
+    options = m.get_items()
+    user_choice = input(f"What would you like? ({options}): ").lower()
+
+    if user_choice == "off":
         is_Off = True
-    elif user_drink == "report":
-        CoffeeMaker.report()
+    elif user_choice == "report":
+        cm.report()
+        mm.report()
     else:
-        if CoffeeMaker.is_resource_sufficient(user_drink) == False:
-            print("“Sorry there is not enough")
+        drink = m.find_drink(user_choice)
+        if cm.is_resource_sufficient(drink) and mm.make_payment(drink.cost):
+            cm.make_coffee(drink)
